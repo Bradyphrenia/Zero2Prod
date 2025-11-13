46dba60c1a4ecde01f597c618901cb56188cf336
steffen <stro889002@aol.com>
2025-11-13 23:26:15 +0100

    Refactor main function to use custom Tokio runtime and add dynamic name route

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@ async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/", web::get().to(greet)))
+
+//#[tokio::main]
+fn main() -> Result<(), std::io::Error> {
+    let body = async move {
+        HttpServer::new(|| {
+            App::new()
+                .route("/", web::get().to(greet))
+                .route("/{name}", web::get().to(greet))
+        })
         .bind("127.0.0.1:8000")?
         .run()
         .await
+    };
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the runtime")
+        .block_on(body)
 }
