c36257a1cbf7560904f21e96009c16f2437328f0
steffen <stro889002@aol.com>
2025-11-14 22:25:09 +0100

    Refactor `run` function to accept a `TcpListener`, update tests to use dynamic ports, and revert edition to 2021

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Listener für einen festen Port erstellen
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
-
-    // run liefert vermutlich ein Future oder Server, den wir awaiten
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     run(listener)?.await
 }
