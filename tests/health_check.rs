23de1ba8b9983ea9b4ceac770057f6151deb8966
steffen <stro889002@aol.com>
2025-11-14 21:00:08 +0100

    Extract server setup to library, add health_check test, and configure project structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -12,6 +12,6 @@ async fn health_check_works() {
 }
 
 fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind adress");
+    let server = zero2prod::run().expect("Failed to bind address");
     let _ = tokio::spawn(server);
 }
